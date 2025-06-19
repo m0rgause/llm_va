63c6cab5aa67edab10909530f3fcbc369ff33aed
m0rgause <antoresf1@gmail.com>
2025-06-19 23:35:56 +0700

    refactor(model): update default model to 'syaki-ai' in getSelectedModel function

--- src/utils/model.ts
+++ src/utils/model.ts
@@ -2,6 +2,7 @@ import { OllamaEmbeddings } from "@langchain/ollama";
 
 export async function getModel() {
   const model = new OllamaEmbeddings({
+    baseUrl: process.env.OLLAMA_URL || "http://localhost:11434",
     model: "mxbai",
   });
 
